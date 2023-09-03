20d71482ac0ab9d30020495b81a83f756340ed54
folcred <robttheb@gmail.com>
2023-09-03 10:14:49 -0600

    Exercise in using .format and random methods
    Shows using format to create aligned columns and using random methods to
    produce variations and unique but repeatable values.

--- testenv.py
+++ testenv.py
@@ -1,9 +1,16 @@
-number_of_bananas = 100
-number_of_bananas += 12
-print( number_of_bananas )
-number_of_bananas -= 13
-print( number_of_bananas )
-number_of_bananas *= 19
-print( number_of_bananas )
-number_of_bananas /= number_of_bananas
-print( (int( number_of_bananas ) ) )
+from random import random, randint, seed
+
+s = "{:>5d} times {:^6.2f} is {:>5.2f}"
+print( s.format( 1, 3.75, 1 * 3.75 ) )
+print( s.format( 2, 3.75, 2 * 3.75 ) )
+print( s.format( 3, 3.75, 3 * 3.75 ) )
+print( s.format( 4, 3.75, 4 * 3.75 ) )
+print( s.format( 5, 3.75, 5 * 3.75 ) )
+
+seed()
+print( "A random number between 1 and 10 is", randint( 1, 10) )
+print( "Another is", randint( 1, 10) )
+seed( 0 )
+print( "3 random numbers are: {:.2f} {:.2f} {:.2f}".format( random(), random(), random() ))
+seed( 0 )
+print( "The same 3 numbers are:", random(), random(), random() )
