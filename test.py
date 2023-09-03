05371787c9256274b4fee5f15b1999e68aab4796
folcred <robttheb@gmail.com>
2023-09-03 13:31:19 -0600

    New file for exercises that constantly change

---
+++ test.py
@@ -0,0 +1,7 @@
+from pcinput import get_integer, get_string, get_float
+
+num1 = get_integer( "Please enter an interger: " )
+new_prompt = get_string( "Please enter a new prompt for a floating point number: " )
+num2 = get_float( new_prompt )
+
+print( "The sum of", num1, "times", num2, "is {:.2f}".format( num1 * num2 ))
