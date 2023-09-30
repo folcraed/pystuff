bbbe7f9e240edf6f0d82882e85beed96210e2477
folcred <robttheb@gmail.com>
2023-09-30 07:49:29 -0600

    A meters to feet calculator
    Calculates meters to feet or feet to meters, depending on the user's
    initial choice.

---
+++ m2f.py
@@ -0,0 +1,18 @@
+"""
+This takes a measurement from the user, in either meters
+or feet and converts it to the other measure.
+"""
+
+conv_which = input("Convert from 'm' meters or 'f' feet? ")
+
+
+if conv_which == "m":
+    meters = int(input("How many meters? "))
+    converted = meters * 3.2808
+    print(f"{meters} meters equal {converted:.1f} feet")
+elif conv_which == "f":
+    feet = int(input("Hom many feet? "))
+    converted = feet * 0.3048
+    print(f"{feet} feet equal {converted:.1f} meters")
+else:
+    print("Can't convert that!")
