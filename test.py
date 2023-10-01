d9412c8f843cf054211f78c43d7d511eaec5a875
folcred <robttheb@gmail.com>
2023-10-01 10:09:52 -0600

    While loop exercise
    Exercise in using a condition in a while loop to force it to exit.

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
-grade = 8.5
+"""
+This is a file for things I'm learning, so it will change constantly.
+If there's something that I would like to save for the future, I will
+save it under a new file name.
+"""
 
-if grade >= 8.5:
-    print("You have an 'A' grade.")
-elif grade >= 7.5:
-    print("You have a 'B' grade.")
-elif grade >= 6.5:
-    print("You have a 'C' grade.")
-elif grade >= 5.5:
-    print("You have a 'D' grade.")
-else:
-    print("You have an 'F' grade. Congratulations, you failed!")
+from pcinput import get_integer
+
+total = 0
+count = 0
+entry = 1
+while entry != 0:
+    prompt = get_integer("Please enter a number, 0 to exit: ")
+    if prompt == 0:
+        print("The total is", total, "and the average is", total / count)
+        exit()
+    count += 1
+    total += prompt
