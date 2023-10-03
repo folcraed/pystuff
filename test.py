2e9b027df895c398fe4199c5a40712e578891854
folcred <robttheb@gmail.com>
2023-10-03 15:17:13 -0600

    More lessons

--- test.py
+++ test.py
@@ -4,22 +4,16 @@ If there's something that I would like to save for the future, I will
 save it under a new file name.
 """
 
-from pcinput import get_integer
+PIRATES = 6
+coconuts = 0
 
-TOTAL = 10
-low = 0
-high = 0
-div_three = 0
-
-for x in range(TOTAL):
-    num = get_integer("Please enter number " + str(x+1) + ": ")
-    if x == 0:
-        low = num
-    if num < low:
-        low = num
-    if num > high:
-        high = num
-    if num % 3 == 0:
-        div_three += 1
-
-print(f"The lowest was {low}, the highest {high}, and {div_three} were divisable by 3.")
+while True:
+    coconuts += 1
+    pile = coconuts
+    for i in range(PIRATES):
+        if pile % PIRATES != 1:
+            break
+        pile = (PIRATES - 1) * int((pile - 1) / PIRATES)
+    if pile % PIRATES == 1:
+        break
+print(coconuts)
