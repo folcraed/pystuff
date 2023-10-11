d122cf6a0b1fda21d10ef2730bb5ea596dbb5730
folcred <robttheb@gmail.com>
2023-10-11 10:22:30 -0600

    Example of a sort-of grammar checker
    Though this worth keeping as it's a good example of how to parse
    text sentences and paragraphs for common mistakes. Uses an example
    paragraph which it runs through finding defined errors, producing
    a fixed copy.

---
+++ fix_words.py
@@ -0,0 +1,57 @@
+"""
+An example of how to search through words in a sentence of paragraph and
+fix errors in spelling found. Think of a very basic spelling or grammar
+checker. The example text of course is known, so the error checking is
+tailored to it. But it does provide ideas for methods of parsing text to
+make corrections.
+"""
+
+text = """
+as it turns out our chance meeting with REverend aRTHUR BElling was was
+to change our whole way of life, and every sunday we'd hurry along to
+St lOONY up the Cream BUn and Jam...
+"""
+
+wordlist = text.split()
+lastword = ""
+newtext = ""
+
+
+for word in wordlist:
+    # Correct any errant capitalization
+    if (
+        len(word) > 2
+        and word[0] >= "A"
+        and word[0] <= "Z"
+        and word[1:] >= "A"
+        and word[1:] <= "Z"
+    ):
+        word = word[0] + word[1:].lower()
+
+    # Capitalize any day of the week reference
+    if word[3:] == "day":
+        word = word[0].upper() + word[1:]
+
+    # Correct any reverse capitalization
+    if word[0] >= "a" and word[0] <= "z":
+        allcaps = True
+        for c in word[1:]:
+            if not (c >= "A" and c <= "Z"):
+                allcaps = False
+                break
+        if allcaps:
+            word = word[0].upper() + word[1:].lower()
+
+    # Spring the trap set below for any duplicated words
+    if word == lastword:
+        continue
+
+    newtext += word + " "
+    # Set a trap for any duplicated words
+    lastword = word
+
+
+newtext = newtext.strip()
+# Ensure first letter of sentence is capitalized
+final = newtext[0].upper() + newtext[1:]
+print(final)
