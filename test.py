2ba42c2c3914da1fc8137e810bc8f907236cfa30
folcred <robttheb@gmail.com>
2023-10-13 10:31:57 -0600

    TTT board for later use

--- test.py
+++ test.py
@@ -4,28 +4,17 @@ If there's something that I would like to save for the future, I will
 save it under a new file name.
 """
 
-from pcinput import get_string
+
+def display_board(b):
+    print("  1 2 3")
+    for row in range(3):
+        print(row + 1, end=" ")
+        for col in range(3):
+            print(b[row][col], end=" ")
+        print()
 
 
-def findCommon(word1, word2):
-    """
-    This takes two words the user enters and compares them to each other,
-    looking for matching characters. The number of matches are returned."""
-    common = ""
-    for letter in word1:
-        if (letter in word2) and (letter not in common):
-            common += letter
-    if common == "":
-        return 0
-    else:
-        return len(common)
-
-
-def main():
-    word1 = get_string("Enter a word: ")
-    word2 = get_string("Enter another word: ")
-    print(f"The words have {findCommon(word1, word2)} letters in common.")
-
-
-if __name__ == "__main__":
-    main()
+board = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
+board[1][1] = "X"
+board[0][2] = "0"
+display_board(board)
