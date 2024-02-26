70bce1055caf8cab0d601fd17efd9e8e326c4168
folcred <robttheb@gmail.com>
2024-02-26 10:40:09 -0700

    Refactored and added docstrings
    Put the user input and format checking into a function that re-prompts
    the user if they made an error and allows them to try again. Moved all
    the functions to the top, and added docstrings to them.

--- algebra_problem.py
+++ algebra_problem.py
@@ -1,7 +1,24 @@
 # Takes a solve-for-x problem from the user and solves it.
 
+
 # First we need to get the problem from the user
-problem = input("Enter the formula to solve: ")
+def get_problem():
+    """Prompts the user for their formula to solve
+    and checks to see if it's properly formatted."""
+    query = input("Enter the formula to solve, or 'q' to quit: ")
+    if query.find("q") != -1:
+        exit()
+    elif query.find(" ") != -1:
+        print("You must not use spaces in the formula, try again!")
+        get_problem()
+    elif query.find("x") == -1:
+        print("You must use 'x' as missing element, try again!")
+        get_problem()
+    return query
+
+
+# We place the user's query in a global variable for processing
+problem = get_problem()
 
 # We create some placeholders for later use, so we don't get an error
 end_of = len(problem)
@@ -10,15 +27,33 @@ op_pos = 0
 num1 = 0
 num2 = 0
 
-# Then we need to make sure they entered a valid problem
-if problem.find(" ") != -1:
-    print("You must not use spaces in the formula, try again!")
-    exit()
-elif problem.find("x") == -1:
-    print("You must use 'x' as missing element, try again!")
-    exit()
 
-# Next we need to find out where x, operator and = are
+# These functions will do the math once we've processed the user's input
+def addup(first, second):
+    """Adds the numbers together to solve for 'x'."""
+    x_is = float(first) + float(second)
+    print("x equals {:.1f}".format(x_is))
+
+
+def subup(first, second):
+    """Subtracts one number from the other to solve for 'x'."""
+    x_is = float(first) - float(second)
+    print("x equals {:.1f}".format(x_is))
+
+
+def divup(first, second):
+    """Divides one number by the other to solve for 'x'."""
+    x_is = float(first) / float(second)
+    print("x equals {:.1f}".format(x_is))
+
+
+def multup(first, second):
+    """Multiplys the numbers together to solve for 'x'."""
+    x_is = float(first) * float(second)
+    print("x equals {:.1f}".format(x_is))
+
+
+# We need to find out where 'x', the operator and '=' are
 # First we look for x, and store it's location
 x_loc = problem.find("x")
 
@@ -44,16 +79,16 @@ equals = problem.find("=")
 if x_loc != 0:
     num1 = problem[0:op_pos]
 else:
-    num1 = problem[(op_pos + 1):equals]
+    num1 = problem[(op_pos + 1) : equals]
 
 
 if x_loc != last_place:
-    num2 = problem[(equals + 1):end_of]
+    num2 = problem[(equals + 1) : end_of]
 else:
-    num2 = problem[(op_pos + 1):equals]
+    num2 = problem[(op_pos + 1) : equals]
 
-# knowing where x is, we turn that into a marker position, 0, 2 or 4
-# This simplifies the later function logic on which to use and how.
+# Knowing where x is, we turn that into a marker position, 0, 2 or 4.
+# This position lets us know what is 'x' and what are numbers.
 if x_loc == last_place:
     x_loc = 4
 elif x_loc == 0:
@@ -61,42 +96,20 @@ elif x_loc == 0:
 else:
     x_loc = 2
 
-
-# We create functions to actually do the math
-def addup(first, second):
-    x_is = float(first) + float(second)
-    print("x equals {:.1f}".format(x_is))
-
-
-def subup(first, second):
-    x_is = float(first) - float(second)
-    print("x equals {:.1f}".format(x_is))
-
-
-def divup(first, second):
-    x_is = float(first) / float(second)
-    print("x equals {:.1f}".format(x_is))
-
-
-def multup(first, second):
-    x_is = float(first) * float(second)
-    print("x equals {:.1f}".format(x_is))
-
-
 # Finally, using our marker position, we create the logic to evaluate x
 if (operator == "+" and x_loc == 0) or (operator == "+" and x_loc == 2):
     subup(num2, num1)
-elif (operator == "+" and x_loc == 4):
+elif operator == "+" and x_loc == 4:
     addup(num1, num2)
-elif (operator == "-" and x_loc == 0):
+elif operator == "-" and x_loc == 0:
     addup(num2, num1)
 elif (operator == "-" and x_loc == 2) or (operator == "-" and x_loc == 4):
     subup(num1, num2)
 elif (operator == "*" and x_loc == 0) or (operator == "*" and x_loc == 2):
     divup(num2, num1)
-elif (operator == "*" and x_loc == 4):
+elif operator == "*" and x_loc == 4:
     multup(num1, num2)
-elif (operator == "/" and x_loc == 0):
+elif operator == "/" and x_loc == 0:
     multup(num2, num1)
 elif (operator == "/" and x_loc == 2) or (operator == "/" and x_loc == 4):
     divup(num1, num2)
