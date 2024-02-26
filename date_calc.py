892f5f70cf9167f55b15dc4484bfb74ce3c6af1c
folcred <robttheb@gmail.com>
2024-02-26 16:02:58 -0700

    Rewrite to make it more usable, more functionality
    It now can calculate the difference between two dates, or take a date
    and a time span and calculate the date difference. Often handy when
    getting dates from gravestones or quickly figuring out someone's age at
    a certain time from their dates.

--- date_calc.py
+++ date_calc.py
@@ -1,18 +1,64 @@
-"""
-This was one of the examples from the lesson book that I think would be very
-handy for calculating genealogical dates. It will need to be expanded upon,
-but is a good starting point, showing how datetime can do date arithmetic.
-"""
-# TODO I want to expand on this later to use for calculating genealogy dates
+# A date calculator for use in genealogy
 
 import datetime
+from dateutil.relativedelta import relativedelta
 
 
-def addDays(year, month, day, increment):
-    startdate = datetime.datetime(year, month, day)
-    enddate = startdate + datetime.timedelta(days=increment)
-    return enddate.year, enddate.month, enddate.day
+def calcdiff():
+    print("Enter the dates as '1899-3-14' (Year, Month, Day) when prompted")
+
+    starting = input("Enter the first date: ")
+    STARTDATE = datetime.datetime.strptime(starting, "%Y-%m-%d")
+
+    ending = input("Enter the second date: ")
+    ENDDATE = datetime.datetime.strptime(ending, "%Y-%m-%d")
+    calculation = relativedelta(ENDDATE, STARTDATE)
+    output = str(calculation)
+    output = output.strip("relativedelta(")
+    output = output.strip(")")
+
+    print()
+    print(
+        "If the results show '+', the second date is later than the first by that amount."
+    )
+    print(
+        "If the results show '-', the second date is earlier than the first by that amount."
+    )
+    print()
+    print(f"The difference from the first date is: {output}")
 
 
-y, m, d = addDays(2015, 11, 13, 55)
-print(f"{y}/{m}/{d}")
+def calcyear():
+    print("Enter the starting date as year, month, day as year-month-day.")
+    date_entered = input("What is the starting date?: ")
+    stdate = datetime.datetime.strptime(date_entered, "%Y-%m-%d")
+
+    print("Now enter the number of years, months and days separated by commas: ")
+    print("NOTE all fields must have a number greater than 0, if 0, enter a 1 instead.")
+    difference = input("Enter the difference: ")
+
+    m_year, m_month, m_day = difference.split(",")
+
+    newy = int(m_year)
+    newm = int(m_month)
+    newd = int(m_day)
+
+    differ = relativedelta(years=newy, months=newm, days=newd)
+    newyear = (stdate - differ).strftime("%Y-%m-%d")
+
+    print(newyear)
+
+
+print("What kind of calculation do you want,")
+print("(d)ifference between dates, or from (t)imespan?")
+print("Enter 'd' for dates, 't' for timespan, anything else to quit.")
+print
+choice = input("Which option?: ")
+
+if choice == "d":
+    calcdiff()
+elif choice == "t":
+    calcyear()
+else:
+    print("See you later!")
+    exit()
