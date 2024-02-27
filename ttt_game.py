51cdc996dde63826ea08d3e2290bf610a4ba1087
folcred <robttheb@gmail.com>
2024-02-27 12:16:28 -0700

    Did some refactoring to fix a few problems
    While it did work, there were a lot of points of error
    and where functions didn't work quite right, so took
    some time to fix it for some more practice.

--- ttt_game.py
+++ ttt_game.py
@@ -4,11 +4,14 @@ A very simple tic-tac-toe game
 
 # Create the board and some default variables
 board = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
-# By default, the game always starts with user "X"
+
+# By default, the game always starts with user "X
 user = "X"
+
 # Initialize row and column variables
 r = 0
 c = 0
+
 # Since there are only 9 spaces in the game, set a limit
 MAXMOVE = 9
 
@@ -24,43 +27,50 @@ def display_board(b):
         print()
 
 
-def get_row_column(r, c):
+def get_row_column(row, col):
     """Gets the placement for the player's token, and places it on
     the board. If the player enters a space already taken, it gives
-    a warning and let's them try again, using recursion."""
+    a warning and lets them try again, using recursion."""
     placement = input("Enter the row and column you choose: ")
-    r = int(placement[0]) - 1
-    c = int(placement[-1]) - 1
-    if board[r][c] != "-":
+    row = int(placement[0]) - 1
+    col = int(placement[-1]) - 1
+    if board[row][col] != "-":
         print("Space already used, try again.")
-        get_row_column(r, c)
+        get_row_column(row, col)
     else:
-        board[r][c] = user
+        board[row][col] = user
 
 
-def switch_player(user):
+def switch_player(player):
     """Switches between the two players."""
-    if user == "X":
-        user = "O"
-        return user
-    elif user == "O":
-        user = "X"
-        return user
+    if player == "X":
+        player = "O"
+        return player
+    elif player == "O":
+        player = "X"
+        return player
 
 
 def winner(b):
     """Checks the board to see if either player has won the game."""
-    for row in range(3):
-        if b[row][0] != "-" and b[row][0] == b[row][1] and b[row][0] == b[row][2]:
-            return True
-    for col in range(3):
-        if b[0][col] != "-" and b[2][col] == b[1][col] and b[0][col] == b[2][col]:
-            return True
-    if b[1][1] == b[0][0] and b[1][1] == b[2][2]:
+    if b[0][0] != "-" and b[0][1] == b[0][0] and b[0][2] == b[0][0]:
         return True
-    if b[1][1] == b[0][2] and b[1][1] == b[2][0]:
+    elif b[1][0] != "-" and b[1][1] == b[1][0] and b[1][2] == b[1][0]:
         return True
-    return False
+    elif b[2][0] != "-" and b[2][1] == b[2][0] and b[2][2] == b[2][0]:
+        return True
+    elif b[0][0] != "-" and b[1][0] == b[0][0] and b[2][0] == b[0][0]:
+        return True
+    elif b[0][1] != "-" and b[1][1] == b[0][1] and b[2][1] == b[0][1]:
+        return True
+    elif b[0][2] != "-" and b[1][2] == b[0][2] and b[2][2] == b[0][2]:
+        return True
+    elif b[0][0] != "-" and b[1][1] == b[0][0] and b[2][2] == b[0][0]:
+        return True
+    elif b[0][2] != "-" and b[1][1] == b[0][2] and b[2][0] == b[0][2]:
+        return True
+    else:
+        return False
 
 
 # Start the game
@@ -68,7 +78,7 @@ move = 0
 while True:
     display_board(board)
     get_row_column(r, c)
-    if winner(board):
+    if winner(board) is True:
         display_board(board)
         print(f"Player {user} has won!")
         break
