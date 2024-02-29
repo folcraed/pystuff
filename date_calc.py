4a3fff2f2725778b4db2b725850d8db4b00bf4f1
folcred <robttheb@gmail.com>
2024-02-29 09:52:17 -0700

    Added options for calculating future or past dates, cleaned output
    There is sometimes a need to calculate into the future, which the app
    didn't have, only into the past. Added another function to do future,
    and did some formatting of how the output and prompts are presented.

--- date_calc.py
+++ date_calc.py
@@ -3,43 +3,75 @@
 import datetime
 from dateutil.relativedelta import relativedelta
 
+# Turns on and off bold text attribute
+b_on = "\033[1;31m"
+b_off = "\033[0m"
+
 
 def calc_diff():
     """Takes two user supplied dates and calculates the differences between them."""
-    print("Enter the dates as '1899-3-14' (Year, Month, Day) when prompted")
+    print("\nEnter the dates as '1899-3-14' (Year, Month, Day) when prompted")
 
     starting = input("Enter the first date: ")
     STARTDATE = datetime.datetime.strptime(starting, "%Y-%m-%d")
 
-    ending = input("Enter the second date: ")
+    ending = input("\nEnter the second date: ")
     ENDDATE = datetime.datetime.strptime(ending, "%Y-%m-%d")
     calculation = relativedelta(ENDDATE, STARTDATE)
     output = str(calculation)
     output = output.strip("relativedelta(")
     output = output.strip(")")
 
-    print()
     print(
-        "If the results show '+', the second date is later than the first by that amount."
+        "\nIf the results show '+', the second date is later than the first by that amount."
     )
     print(
         "If the results show '-', the second date is earlier than the first by that amount."
     )
-    print()
-    print(f"The difference from the first date is: {output}")
+
+    print(f"\nThe difference from the first date is: {output}")
 
 
-def calc_date():
+def future_calc_date():
+    """Takes a user entered date, prompts for a difference in 'years,months,days' format,
+    and calculates the new date by adding the difference from the original date.
+    """
+    print("\nEnter the starting date as year, month, day as year-month-day.")
+    date_entered = input("\nWhat is the starting date?: ")
+    stdate = datetime.datetime.strptime(date_entered, "%Y-%m-%d")
+
+    print("\nNow enter the number of years, months and days separated by commas: ")
+    print(
+        f"{b_on}NOTE{b_off} all fields must have a number greater than 0, if 0 enter a 1 instead."
+    )
+    difference = input("\nEnter the difference: ")
+
+    m_year, m_month, m_day = difference.split(",")
+
+    newy = int(m_year)
+    newm = int(m_month)
+    newd = int(m_day)
+
+    differ = relativedelta(years=newy, months=newm, days=newd)
+    newyear = (stdate + differ).strftime("%Y-%m-%d")
+
+    print(f"\nThe date is {newyear}.")
+
+
+def past_calc_date():
     """Takes a user entered date, prompts for a difference in 'years,months,days' format,
     and calculates the new date by subtracting the difference from the original date.
     """
-    print("Enter the starting date as year, month, day as year-month-day.")
-    date_entered = input("What is the starting date?: ")
+    print("\nEnter the starting date as year, month, day as year-month-day.")
+    date_entered = input("\nWhat is the starting date?: ")
     stdate = datetime.datetime.strptime(date_entered, "%Y-%m-%d")
 
-    print("Now enter the number of years, months and days separated by commas: ")
-    print("NOTE all fields must have a number greater than 0, if 0, enter a 1 instead.")
-    difference = input("Enter the difference: ")
+    print("\nNow enter the number of years, months and days separated by commas: ")
+    print(
+        f"{b_on}NOTE{b_off} all fields must have a number greater than 0, if 0 enter a 1 instead."
+    )
+
+    difference = input("\nEnter the difference: ")
 
     m_year, m_month, m_day = difference.split(",")
 
@@ -50,19 +82,23 @@ def calc_date():
     differ = relativedelta(years=newy, months=newm, days=newd)
     newyear = (stdate - differ).strftime("%Y-%m-%d")
 
-    print(newyear)
+    print(f"\nThe date is {newyear}.")
 
 
-print("What kind of calculation do you want,")
-print("(d)ifference between dates, or from (t)imespan?")
-print("Enter 'd' for dates, 't' for timespan, anything else to quit.")
-print
-choice = input("Which option?: ")
+print("What kind of calculation do you want, enter letter of choice:\n")
+print(
+    """(d) difference between dates
+(f) for span into future
+(p) for span into past\n"""
+)
+choice = input("Which option do you want?: ")
 
 if choice == "d":
     calc_diff()
-elif choice == "t":
-    calc_date()
+elif choice == "f":
+    future_calc_date()
+elif choice == "p":
+    past_calc_date()
 else:
     print("See you later!")
     exit()
